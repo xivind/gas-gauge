e06ba45cfd7005c02a744e56ccaa9551157a5cd5
Xivind <oivind.langeland@gmail.com>
2025-11-15 13:23:03 +0100

    Updated seed data with new canisters

--- seed_data.py
+++ seed_data.py
@@ -7,6 +7,8 @@ logger = logging.getLogger(__name__)
 
 PREDEFINED_TYPES = [
     {"name": "Coleman 240g", "full_weight": 361, "empty_weight": 122},
+    {"name": "Primus 230g", "full_weight": 381, "empty_weight": 151},
+    {"name": "Primus 100g", "full_weight": 203, "empty_weight": 103}
 ]
 
 def seed_canister_types():
